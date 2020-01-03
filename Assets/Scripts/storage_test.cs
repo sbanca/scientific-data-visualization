9c5c4b22bf6e166b2eecb25afe477a9115d8ed3b
Riccardo Bovo <riccardo.bovo.design@gmail.com>
2020-01-03 18:56:14 +0000

    finally the GLTF is working for importing files in to quest

--- Assets/Scripts/storage_test.cs
+++ Assets/Scripts/storage_test.cs
@@ -1,4 +1,147 @@
-﻿/*using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+using Siccity.GLTFUtility;
+using System.IO;                                                        
+
+
+public class storage_test : MonoBehaviour
+{
+ 
+
+    // Start is called before the first frame update
+    IEnumerator  Start()
+    {
+        
+    string filelisturl = "https://europe-west2-gltf-storage.cloudfunctions.net/fileList";
+    string fileurl = "https://europe-west2-gltf-storage.cloudfunctions.net/fileUrl";
+
+    CoroutineWithData list = new CoroutineWithData(this, GetTextFromUrl(filelisturl) );
+    
+    yield return list.coroutine;
+
+    var objectlist = JsonUtility.FromJson<listjson>(list.result.ToString());
+
+    var smallobjectlist = new List<string> {objectlist.list[0],objectlist.list[1]};
+    
+   
+    //file one 
+
+    CoroutineWithData fileDownloadUrl1 = new CoroutineWithData(this, GetTextFromUrl(fileurl,"filename",objectlist.list[0]) );
+
+    yield return fileDownloadUrl1.coroutine;
+
+    Debug.Log("file is => "+objectlist.list[0] + " url is =>" + fileDownloadUrl1.result);
+
+    
+    CoroutineWithData fileDownload1 = new CoroutineWithData(this, DownloadFileFromUrl( fileDownloadUrl1.result.ToString() ,objectlist.list[0]) );
+
+    yield return fileDownload1.coroutine;
+
+    Debug.Log("file  => "+objectlist.list[0] + " is stored at =>" + fileDownload1.result);
+
+
+    
+    //file two
+    
+    CoroutineWithData fileDownloadUrl2 = new CoroutineWithData(this, GetTextFromUrl(fileurl,"filename",objectlist.list[1]) );
+
+    yield return fileDownloadUrl2.coroutine;
+
+    Debug.Log("file is => "+objectlist.list[0] + " url is =>" + fileDownloadUrl2.result);
+
+    
+    CoroutineWithData fileDownload2 = new CoroutineWithData(this, DownloadFileFromUrl( fileDownloadUrl2.result.ToString() ,objectlist.list[1]) );
+
+    yield return fileDownload2.coroutine;
+
+    Debug.Log("file  => "+objectlist.list[0] + " is stored at =>" + fileDownload2.result);
+
+
+    //load 
+
+    
+    GameObject gltfObject = Siccity.GLTFUtility.Importer.LoadFromFile(fileDownload2.result.ToString()); 
+     
+   
+    }
+    
+    IEnumerator GetTextFromUrl(string url, string ParameterName = "", string ParameterValue = "") {
+       
+        if ( ParameterName != "" )   url = url + "?" + ParameterName + "=" +ParameterValue;
+        
+        Debug.Log("Start Downalod: " + url);
+
+        UnityWebRequest www = new UnityWebRequest(url);
+        www.downloadHandler = new DownloadHandlerBuffer();
+        
+        yield return www.SendWebRequest();
+
+        if(www.isNetworkError || www.isHttpError) {
+            Debug.Log(www.error);
+        }
+        else {
+            yield return www.downloadHandler.text;                       
+        }
+    }
+
+    IEnumerator DownloadFileFromUrl(string url, string filename) {
+        
+        Debug.Log("Downalod from: " + url);
+        
+        string local_file = System.IO.Path.Combine(Application.persistentDataPath,filename);
+
+        Debug.Log("Save to" + local_file);
+
+        UnityWebRequest www = new UnityWebRequest(url);
+        www.downloadHandler = new DownloadHandlerBuffer();
+        
+        yield return www.SendWebRequest();
+
+        if(www.isNetworkError || www.isHttpError) {
+            Debug.Log(www.error);
+        }
+        else {
+
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+
+            File.WriteAllBytes(local_file, results);
+
+            yield return local_file;
+                     
+        }
+    }
+
+
+}
+
+
+[Serializable]
+public class listjson
+{
+    public List<string> list;
+}
+
+ public class CoroutineWithData {
+     public Coroutine coroutine { get; private set; }
+     public object result;
+     private IEnumerator target;
+     public CoroutineWithData(MonoBehaviour owner, IEnumerator target) {
+         this.target = target;
+         this.coroutine = owner.StartCoroutine(Run());
+     }
+ 
+     private IEnumerator Run() {
+         while(target.MoveNext()) {
+             result = target.Current;
+             yield return result;
+         }
+     }
+ }
+ /*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Firebase.Storage;
