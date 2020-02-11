273ba7d9b6954a2802398e85f66ca78caa44023e
sbanca <riccardo.bovo.design@gmail.com>
2020-02-11 18:17:30 +0000

    arrow preset

---
+++ Assets/Scripts/arrowController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrowController : MonoBehaviour
+{
+    Material mat;
+    Color target = Color.red;
+    Color orgcol;
+
+    void Awake()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+        orgcol = mat.color;
+
+    }
+
+    void OnMouseEnter()
+    {
+        mat.color = target;
+    }
+
+    void OnMouseExit()
+    {
+        mat.color = orgcol;
+    }
+
+    void OnMouseUpAsButton()
+    {
+        Destroy(this.gameObject);
+    }
+
+
+
+}
+
