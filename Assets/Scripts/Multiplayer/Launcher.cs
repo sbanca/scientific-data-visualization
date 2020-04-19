a797626f56cee81eebd2b326f153e164a7ad6dd3
unknown <rb1619@WIN.DOC.IC.AC.UK>
2020-04-19 20:39:38 +0100

    sample_scene_networking  add launcher
    testing connection to PUN

---
+++ Assets/Scripts/Multiplayer/Launcher.cs
@@ -0,0 +1,27 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+   
+    void Start()
+    {
+        Debug.Log("[PUN] connecting to server");
+        PhotonNetwork.GameVersion = "0.0.1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster() {
+
+        Debug.Log("[PUN] connected to server");
+
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("[PUN] disconnected from server because of " + cause.ToString());
+    }
+}
